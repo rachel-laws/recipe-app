12df25024c7f057d84ca307690c7a241a73afec7
[rachel-laws] <[rlawson13l@gmail.com]>
2023-04-20 23:10:13 -0400

    Fix fade out effect

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,16 +3,20 @@ import Home from './Home';
 import Cuisines from './Cuisines';
 import SearchResults from './SearchResults';
 import Recipes from './Recipes';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 function Pages() {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/cuisines/:type' element={<Cuisines />} />
-      <Route path='/searchresults/:search' element={<SearchResults />} />
-      <Route path='/recipes/:name' element={<Recipes />} />
-    </Routes>
+    <AnimatePresence mode='wait'>
+      <Routes Location={location} key={location.pathname}>
+        <Route path='/' element={<Home />} />
+        <Route path='/cuisines/:type' element={<Cuisines />} />
+        <Route path='/searchresults/:search' element={<SearchResults />} />
+        <Route path='/recipes/:name' element={<Recipes />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
